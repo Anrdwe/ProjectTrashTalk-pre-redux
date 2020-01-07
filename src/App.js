369e51cc1648a685d26a932c12c3039af913ed82
Andrew Wu <andrew_0564@hotmail.com>
2020-01-07 18:32:19 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 //components
 import Nav from './components/Nav';
-import AuthRoute from './util/AuthRoute';
-import UnauthRoute from './util/UnauthRoute';
 //pages
 import About from './pages/About';
 import Home from './pages/Home';
@@ -20,34 +18,9 @@ import Axios from 'axios';
 Axios.defaults.baseURL = 'https://us-east1-trashtalk-245817.cloudfunctions.net/api'
 
 class App extends React.Component {
-  // constructor() {
-  //   super()
-  //   this.state =  {
-  //     authenticated: false
-  //   }
-  // }
-  // setAuth(props) {
-  //   this.setState({authenticated: props})
-  // }
-  // componentDidMount() {
-  //   const token = localStorage.FBIdToken;
-  //   if(token) {
-  //     //we have token, we want to decode it with jwt-decode, inside there is a expiration date
-  //     //the exp value is in seconds and can be passed into date() to get the exact time the token expires
-  //     const decodedToken = jwtDecode(token);
-  //     if(decodedToken.exp * 1000 < Date.now()) {
-  //       //if the token is expired, we redirect them to the login page
-  //       this.setState({ authenticated: false })
-  //       //window.location.href = '/login'
-  //     } else {
-  //       this.setState({ authenticated: false })
-  //     }
-  //   }
-  // }
   
   render() {
   return (
-    //everything inside Provider will have access to store, which is our state
     <Router>
       <div className="App">
         <Nav />
@@ -56,16 +29,13 @@ class App extends React.Component {
           <Route path="/board" component={Board} />
           <Route path="/about" component={About} />
           {//if the user is not authenticated the '/post' route will go to '/login'
-          //<Route path="/post" component={Post} authenticated={this.state.authenticated}/>
-  }
+          }
           <Route path="/post" component={Post}/>
           {//if the user is already authenticated the '/login' and '/signup' route will go to '/board'
           } 
-          {//<AuthRoute path="/login" component={Login} authenticated={this.state.authenticated}/>
-  }
           <Route path="/login" component={Login}/>
           {//<AuthRoute path="/signup" component={Signup} authenticated={this.state.authenticated}/>
-  }
+          }
           <Route path="/signup" component={Signup}/>
         </Switch>
       </div>
